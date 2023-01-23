e5c69e791a920f1ce6cd78e4c249de2fe3d98ed6
angiemera12 <123202355+angiemera12@users.noreply.github.com>
2023-01-23 10:35:54 -0500

    Update main.js

--- main.js
+++ main.js
@@ -11,6 +11,7 @@ document.querySelector('#app').innerHTML = `
       <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
     </a>
     <h1>Hello Vite!</h1>
+     <h1>¿Como estas?</h1>
     <div class="card">
       <button id="counter" type="button"></button>
     </div>
